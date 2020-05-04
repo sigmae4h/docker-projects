6e85c7cfd7597c35d3646a6da8fe539e2bd3ff9f
Sigma <sigma.e4h@gmail.com>
2020-05-04 19:06:39 -0400

    Separate config from image and cleanup

---
+++ ark-server/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -x
+
+export ARK_DIR="${HOME}/ARK"
+
+# Run the server
+${ARK_DIR}/ShooterGame/Binaries/Linux/ShooterGameServer "${MAP}?listen?SessionName=${SERVER_NAME}?ServerPassword=${SERVER_PASSWORD}?AltSaveDirectoryName=../../../saves?ServerAdminPassword=${ADMIN_PASSWORD}?Port=${PORT}?QueryPort=${QUERY_PORT}?RCONPort=${RCON_PORT}?GameModIds=${MODS}" -NoTransferFromFiltering -clusterid=${CLUSTER_NAME} -ClusterDirOverride=${HOME}/clusterdata
