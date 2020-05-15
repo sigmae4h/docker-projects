7529e58673f6e14785d750b84844583539bc64bd
Sigma <sigma.e4h@gmail.com>
2020-05-15 08:21:15 -0400

    Enable rcon

--- ark-server/run.sh
+++ ark-server/run.sh
@@ -5,4 +5,4 @@ set -x
 export ARK_DIR="${HOME}/ARK"
 
 # Run the server
-${ARK_DIR}/ShooterGame/Binaries/Linux/ShooterGameServer "${MAP}?listen?SessionName=${SERVER_NAME}?ServerPassword=${SERVER_PASSWORD}?AltSaveDirectoryName=../../../saves?ServerAdminPassword=${ADMIN_PASSWORD}?Port=${PORT}?QueryPort=${QUERY_PORT}?RCONPort=${RCON_PORT}?GameModIds=${MODS}" -NoTransferFromFiltering -clusterid=${CLUSTER_NAME} -ClusterDirOverride=${HOME}/clusterdata
+${ARK_DIR}/ShooterGame/Binaries/Linux/ShooterGameServer "${MAP}?listen?SessionName=${SERVER_NAME}?ServerPassword=${SERVER_PASSWORD}?AltSaveDirectoryName=../../../saves?ServerAdminPassword=${ADMIN_PASSWORD}?Port=${PORT}?QueryPort=${QUERY_PORT}?RCONEnabled=True?RCONPort=${RCON_PORT}?GameModIds=${MODS}" -NoTransferFromFiltering -clusterid=${CLUSTER_NAME} -ClusterDirOverride=${HOME}/clusterdata
