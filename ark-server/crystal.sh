b0d98c20b0a16ce08b851089a3a35d3140fe764d
Sigma <sigma.e4h@gmail.com>
2020-10-24 22:31:48 -0400

    Update configs

--- ark-server/crystal.sh
+++ ark-server/crystal.sh
@@ -7,7 +7,7 @@ docker run \
     -p 7777:7777/udp \
     -p 7778:7778/udp \
     -p 27015:27015/udp \
-    -e "MAP=TheIsland" \
+    -e "MAP=Crystalisles" \
     -v ArkClusterData:/home/steam/clusterdata \
     -v ArkSaves:/home/steam/saves \
     ark-base
